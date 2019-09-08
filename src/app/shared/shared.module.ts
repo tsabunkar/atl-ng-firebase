48f78efb7b78ed74f0962d58ba507640fef4be1a
Tejas Sabunkar <tsabunkar@gmail.com>
2019-09-08 20:11:23 +0530

    styles and files added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TextboxComponent } from './components/textbox/textbox.component';
-
-
+import { MaterialModule } from './modules/material.module';
 
 @NgModule({
   declarations: [TextboxComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, MaterialModule],
+  exports: [MaterialModule]
 })
-export class SharedModule { }
+export class SharedModule {}
