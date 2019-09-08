dab7a54c443645ac7393a2dfefa821fc3d1c18dc
Tejas Sabunkar <tsabunkar@gmail.com>
2019-09-08 19:26:35 +0530

    folder struc added

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TextboxComponent } from './components/textbox/textbox.component';
+
+
+
+@NgModule({
+  declarations: [TextboxComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
